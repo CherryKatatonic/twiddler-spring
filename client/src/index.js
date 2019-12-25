2c2c073877bbf7649c2dfb352c762bd66d061aa0
Kat Grennan <caitlyn.grennan@techtonic.com>
2019-12-25 16:11:55 -0700

    Comment out dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
+// import { config } from "dotenv";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
